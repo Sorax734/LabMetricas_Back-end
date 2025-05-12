75204cb5150ed9d83040a2966cca3f3b3b18be05
Antonio7347 <20233tn106@utez.edu.mx>
2025-05-12 12:44:03 -0600

    Inicio de proyecto

---
+++ src/test/java/com/labMetricas/LabMetricas/LabMetricasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labMetricas.LabMetricas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabMetricasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
